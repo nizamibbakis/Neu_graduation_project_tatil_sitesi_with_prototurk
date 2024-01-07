ca59ac21264b7360b43d975c5a09fb5dda8ece3a
katilkomsu <nbarisbakis@gmail.com>
2024-01-07 20:30:28 +0300

    denemeler_ogrenmeler_1

--- src/App.js
+++ src/App.js
@@ -1,66 +1,55 @@
-import { useState } from "react";
-
-function reducer(state,action){
-  switch(action.type){
-    case 'SET_TODO':
-      return{
-        ...state,
-        todo:action.value
-      }
-      case 'ADD_TODO':
-        return{
-          ...state,
-          todo:'',
-          todos:[
-            ...state.todos,
-            action.todo
-        ]
-        }
-  }
-}
+import { useMemo,useState } from "react";
 
 function App() {
-  // console.log('app rendered')
 
-  const [state, dispatch] = useReducer(reducer,{
-    todos:[],
-    todo:''
-  });
-  const submitHandle=e=>{
-    e.preventDefault()
-    dispatch({
-      type:'ADD_TODO',
-      todo:state.todo
-    })
-    // setTodos([...todos,todo])
-    // setTodo('')
-  }
-const onChange=e=>{
-  dispatch({
-    type:'SET_TODO',
-    value:e.target.value
-  }) 
-}
+  const genders=[
+    {key:'1',value:'erkek'},
+    {key:'2',value:'kadin'},
+  ]
+  const categoryList=[
+    {key:'1',value:'php'},
+    {key:'2',value:'javascript'},
+    {key:'3',value:'css'},
+    {key:'4',value:'html'},
+  ]
+  const [name,setName]=useState('baris')
+  const [description,setDescription]=useState('')
+  const [gender,setGender]=useState('')
+  const [categories,setCategories]=useState([])
 
-  
+  const selectedGender=genders.find(g=>g.key==gender)
+
+
   return (
     <>
-    <h1>Todo App</h1>
-    <form onSubmit={submitHandle}>
-      <input type="text" value={state.todo} onChange={onChange} />
-      
-      <button disabled={!state.todo} type="submit">ekle</button>
-    </form>
-    <ul>
-      {state.todos.map((todo,index)=>(
-        <li key={index}>
-          {todo}
-        </li>
-      ))}
-    </ul>
+    <button onClick={()=>setName('ahmet')}>adi degistir</button>
+    <input type="text" value={name} onChange={e=>setName(e.target.value)} /><br />
 
-    </>    
-    );
+    <textarea value={description} onChange={e=>setDescription(e.target.value)}></textarea>
+    ad:{name}
+    <br />
+    desc:{description}
+
+    <select value={gender} onChange={e=>setGender(e.target.value)}>
+      <option value=''>seçin</option>
+      {genders.map((gender)=>(
+        <option value={gender.key} key={gender.key}>{gender.value}</option>
+      ))}
+    </select ><br /><br /><br />
+
+    <select value={categories} multiple={true} onChange={e=>setCategories([...e.target.selectedOptions].map(option=>parseInt(option.value)))}>
+      <option value=''>seçin</option>
+      {categoryList.map((category)=>(
+        <option value={category.key} key={category.key}>{category.value}</option>
+      ))}
+    </select ><br /><br /><br />
+
+    {/* cinsiyet:{selectGender} */}
+
+    <pre>{JSON.stringify(categories,null,2)}</pre>
+    
+    </>
+  )
       }
 
 export default App;
